5775310663db295e14903555d982b4b98866a1b0
ALAN KARLO MANGUBAT <karlomangubat92@gmail.com>
2020-12-11 20:04:27 -0800

    Finished code challenge 2

--- python/challenges/array_shift/array_shift/array_shift.py
+++ python/challenges/array_shift/array_shift/array_shift.py
@@ -1,17 +1,23 @@
-test = [3,4,5,6,7]
-insert_value = 20
-index = 2 
+# def array_length(arr):
+#     return len(test)
 
-def array_length(arr):
-    return len(test)
+# def insert_number(arr, index, number):
+#   arr.insert(index, number)
+#   return arr
 
-def insert_number(arr, index, number):
-  arr.insert(index, number)
+def insertShiftArray(arr, num):
+  array_length = len(arr)
+
+  if array_length % 2:
+    middle_array = round(array_length // 2) + 1
+  else:
+    middle_array = array_length // 2
+
+  arr.insert(middle_array, num)
   return arr
+
 
 # print(insert_number(test, index, insert_value))
 # input('> ')
 # Determine first if it's odd or even
 # Insert the number to the left 
-
-
