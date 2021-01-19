40c787a786a112b4cbbdb26b67f6f2ffcfdea926
ALAN KARLO MANGUBAT <karlomangubat92@gmail.com>
2021-01-18 21:18:37 -0800

    Finished Code Challenge 13.

--- python/challenges/tests/test_queue_with_stacks.py
+++ python/challenges/tests/test_queue_with_stacks.py
@@ -1,13 +1,6 @@
 import pytest
 from challenges.queue_with_stacks.queue_with_stacks import PseudoQueue, Stack, Node, InvalidOperationError
 
-def test_enqueue_from_example():
-    pq = PseudoQueue()
-    pq.enqueue(5)
-    actual = pq.stack1.top.value
-    expected = 5
-    assert actual == expected
-
 def test_dequeue_from_example():
     pq = PseudoQueue()
     pq.enqueue(5)
@@ -16,3 +9,5 @@ def test_dequeue_from_example():
     actual = pq.dequeue()
     expected = 5
     assert actual == expected
+
+    
