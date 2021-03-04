8d5e2d015b0fbc3f3e74ddb9ac1f2621b2afceb8
ALAN KARLO MANGUBAT <karlomangubat92@gmail.com>
2021-03-03 20:15:00 -0800

    Implemented code and tests for insertion sort

--- python/challenges/insertion_sort/insertion_sort.py
+++ python/challenges/insertion_sort/insertion_sort.py
@@ -1,3 +1,11 @@
 def insertion_sort(arr):
-    pass
-
+  index_length = range(1, len(arr))    
+  for i in index_length:
+    right = arr[i]
+       
+    while arr[i-1] > right and i>0:
+      arr[i], arr[i-1] = arr[i-1], arr[i]
+      i -= 1
+    
+  return arr
+            
