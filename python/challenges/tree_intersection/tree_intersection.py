e2581068bbd7bc670bfdb950a41ae3f27d867b92
ALAN KARLO MANGUBAT <karlomangubat92@gmail.com>
2021-03-19 16:31:27 -0700

    Cleaned DRY code

--- python/challenges/tree_intersection/tree_intersection.py
+++ python/challenges/tree_intersection/tree_intersection.py
@@ -10,50 +10,28 @@ class BinaryTree:
 
 def tree_intersection(tree1,tree2):
     intersection = []
-    list_tree_1 = []
-    list_tree_2 = []
-
-    if tree1.root is None:
-        return list_tree_1
-    else:
-        def traverse1(root):
-            list_tree_1.append(root.value)
-            if root.left:
-                traverse1(root.left)
-            if root.right:
-                traverse1(root.right)
-        
-        traverse1(tree1.root)
-
-    if tree2.root is None:
-        return list_tree_2
-    else:
-        def traverse2(root):
-            list_tree_2.append(root.value)
-            if root.left:
-                traverse2(root.left)
-            if root.right:
-                traverse2(root.right)
-
-        traverse2(tree2.root)
-# [1, 1, 2]
-# [2]
-
-#[1]
+    list_tree_1 = pre_order(tree1)
+    list_tree_2 = pre_order(tree2)
+    
     for i in range(len(list_tree_1)):
         if list_tree_1[i] in list_tree_2:
             intersection.append(list_tree_1[i])
             list_tree_2.remove(list_tree_1[i])
     return intersection
 
-def traverse(root):
-    list_tree = []
-    list_tree_.append(root.value)
-    if root.left:
-        traverse(root.left)
-    if root.right:
-        traverse(root.right)
-    
+def pre_order(tree):
+        pre_order_list = []
+        if tree.root is None:
+            return []
+        def traverse(root):
+            pre_order_list.append(root.value)
+            if root.left:
+                traverse(root.left)
+            if root.right:
+                traverse(root.right)
+        traverse(tree.root)
+        return pre_order_list
+        
 if __name__ == "__main__":
     a = Node("A")
     b = Node("B")
@@ -93,4 +71,4 @@ if __name__ == "__main__":
     # print(a)
     # print(b)
     # print(c)
-    # print(tree_intersection(tree3,tree2))
+    print(tree_intersection(tree3,tree2))
